6aff457358c794fa96cc37a629e7341af58b0663
Evolution-uc <uchennau659@gmail.com>
2024-07-18 09:02:04 -0700

    uchenna

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -23,20 +23,7 @@ function Contact() {
       d="M6 18L18 6M6 6l12 12" />
   </svg>
 </button>
-{/* <button className="btn btn-circle btn-outline">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor">
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M6 18L18 6M6 6l12 12" />
-  </svg>
-</button> */}
+
         <h1 className=" text-center font-semibold border-b-8 ">
           log in or sign up
         </h1>
